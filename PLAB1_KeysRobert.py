3ef4f6ec3b3dafa04b350fab2c724cbd9e8db8a2
LlavezTheFirst <llavezthefirst@gmail.com>
2024-09-18 18:23:32 -0400

    Add files via upload
    First submnission.

---
+++ PLAB1_KeysRobert.py
@@ -0,0 +1,9 @@
+#Robert Keys
+#09/18/2024
+#PLAB1
+#First program built by me. Name: Name Robot.
+
+first_name=input("Enter your first name:")
+last_name=input("Enter your last name:")
+
+print(f"Hello, {first_name} {last_name}! Welcome to CTI-110")
